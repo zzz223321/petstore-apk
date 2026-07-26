11716fb28dcfc6f8c1554ece6d04ea492049f17a
zzz223321 <1878956748@qq.com>
2026-07-26 10:55:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,5 +16,5 @@ def create_app():
     return app
 
 if __name__ == '__main__':
-    application = create_app()
-    application.run(host='0.0.0.0', port=5000, debug=True)
+    app = create_app()
+    app.run(host='0.0.0.0', port=5000, debug=True)
