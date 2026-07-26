ed093a75edc829d9b9f432f5c96a1745411c6b89
zzz223321 <1878956748@qq.com>
2026-07-26 10:38:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import threading
-import webview
+import time
+from android import Android
 from app import create_app
 
 def start_flask():
-    application = create_app()
-    application.run(host='127.0.0.1', port=5000, debug=False)
+    app = create_app()
+    app.run(host='127.0.0.1', port=5000, debug=False)
+
+def open_browser():
+    time.sleep(3)
+    android = Android()
+    android.start_activity('android.intent.action.VIEW', 'http://127.0.0.1:5000')
 
 if __name__ == '__main__':
     threading.Thread(target=start_flask, daemon=True).start()
-    webview.create_window('宠物店管家', 'http://127.0.0.1:5000')
-    webview.start()
+    threading.Thread(target=open_browser, daemon=True).start()
+    # 保持主线程活着
+    while True:
+        time.sleep(1)
